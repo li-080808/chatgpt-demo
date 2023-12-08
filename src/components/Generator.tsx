041a21b767eea8be565607b0475c2cd69f7f9935
li-080808 <150790246+li-080808@users.noreply.github.com>
2023-12-08 23:57:41 +0800

    Update Generator.tsx

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -233,7 +233,7 @@ export default () => {
         when={!loading()}
         fallback={() => (
           <div class="gen-cb-wrapper">
-            <span>AI is thinking...</span>
+            <span>思考中...</span>
             <div class="gen-cb-stop" onClick={stopStreamFetch}>Stop</div>
           </div>
         )}
@@ -243,7 +243,7 @@ export default () => {
             ref={inputRef!}
             disabled={systemRoleEditing()}
             onKeyDown={handleKeydown}
-            placeholder="Enter something..."
+            placeholder="你好"
             autocomplete="off"
             autofocus
             onInput={() => {
@@ -254,7 +254,7 @@ export default () => {
             class="gen-textarea"
           />
           <button onClick={handleButtonClick} disabled={systemRoleEditing()} gen-slate-btn>
-            Send
+            发送
           </button>
           <button title="Clear" onClick={clear} disabled={systemRoleEditing()} gen-slate-btn>
             <IconClear />
